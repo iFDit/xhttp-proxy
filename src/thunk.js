2542ef20512391de017de97a67fbfded9cfe7c03
iFdit <lyliyangwc@gmail.com>
2018-03-29 19:51:48 +0800

    add xhr reponse and request stream handle

--- src/thunk.js
+++ src/thunk.js
@@ -6,13 +6,22 @@ const without = util.without
 const responseCache = {}
 
 function thunk(readstream, writestream) {
-  readystream.subscribe()
+  readstream.subscribe(handleHttpReadStream)
+  // readstream.subscribe(function (data) {
+  //   console.log('thunk', this)
+  //   console.log('thunk', data)
+  // })
+  writestream.subscribe(handleHttpWriteStream)
 }
 
 function handleHttpReadStream(bundle) {
-  const request = bundle.data
+  const request = bundle.data || {}
   const error = bundle.error
-  const meta = bundle.meta
+  const meta = request.meta
+  const sendData = request.data
+  const url = request.url
+  const method = request.method
+  const headers = request.headers
 
   if (error) {
     // handle error
@@ -20,16 +29,27 @@ function handleHttpReadStream(bundle) {
   }
 
   if (meta.type === 'XHR') {
-    if (!context || context.aborted) { return }
     const context = meta.ctx
+    if (!context || context.aborted || context.sended) { return }
+    context.triggerEvent('statechange')
+    context.originOpen(method, url)
+    context.changestate(STATE['OPENED'])
+    context.triggerEvent('statechange')
+    handleHeaders(headers, context)
     context.originSend(request.data)
+    context.sended = true
   }
 }
 
-function handleHttpWriteStream(data) {
-  const response = bundle.data
+function handleHeaders(headers, ctx) {
+  const keys = Object.keys(headers)
+  keys.forEach((key) => ctx.originSetRequestHeader(key, headers[key]))
+}
+
+function handleHttpWriteStream(bundle) {
+  const response = bundle.data || {}
   const error = bundle.error
-  const meta = bundle.meta
+  const meta = response.meta
 
   if (error) {
     // handle error
@@ -37,48 +57,102 @@ function handleHttpWriteStream(data) {
   }
 
   if (meta.type === 'XHR') {
-    if (!context || context.aborted) { return }
+    const withoutProps = ['readystate', 'meta']
     const context = meta.ctx
+    if (!context || context.aborted) { return }
     const id = context.id
     const readystate = response.readystate
     const cache = responseCache[id] || (responseCache[id] = {})
 
     if (readystate === STATE['HEADERS_RECEIVED']) {
-      context.mergeRespose(without(response, 'readystate'))
-      context.triggerEvent('statechange')
-      cache[STATE['HEADERS_RECEIVED']] = { done: true }
+      const receive = cache[STATE['HEADERS_RECEIVED']] || (cache[STATE['HEADERS_RECEIVED']] = {})
+      const res = without(response, withoutProps)
+      handleHeaderRecerved(receive, context, res)
     }
 
     if (readystate === STATE['LOADING']) {
       const receive = cache[STATE['HEADERS_RECEIVED']]
-      const res = without(response, 'readystate')
+      const loading = cache[STATE['LOADING']] || (cache[STATE['LOADING']] = {})
+      const res = without(response, withoutProps)
+      const data = loading.data || (loading.data = [])
       if (receive && receive.done) {
-        context.mergeRespose(res)
-        context.triggerEvent('statechange')
+        handleLoading(loading, context, data.concat(res))
       } else {
-        cache[STATE['HEADERS_RECEIVED']] = { done: false, data: res }
+        data.push(res)
+        loading = { done: false, data }
       }
     }
 
     if (readystate === STATE['DONE']) {
       const loading = cache[STATE['LOADING']]
-      
+      const done = cache[STATE['DONE']]
+      const res = without(response, withoutProps)
+      if (loading && loading.done) {
+        handleDone(done, context, res)
+      } else {
+        done = { done: false, data: res }
+      }
     }
   }
 }
 
+function handleHeaderRecerved(cache, ctx, response) {
+  ctx.changestate(STATE['HEADERS_RECEIVED'])
+  ctx.mergeResponse(response)
+  ctx.triggerEvent('statechange')
+  cache.done = true
+  checkCache(STATE['HEADERS_RECEIVED'], cache, response, ctx)
+}
 
-function checkCache(readystate, cache, res, context) {
+function handleLoading(cache, ctx, response) {
+  const res = response || cache.data
+  if (!res) { return }
+  ctx.changestate(STATE['LOADING'])
+  res.forEach((newRes) => {
+    ctx.mergeResponse(newRes)
+    ctx.triggerEvent('statechange')
+    checkCache(STATE['LOADING'], cache, newRes, ctx)
+  })
+  cache.done = true
+  cache.data = []
+}
+
+function handleDone(cache, ctx, response) {
+  const res = response || cache.data
+  if (!res) { return }
+  ctx.changestate(STATE['DONE'])
+  ctx.mergeResponse(res)
+  ctx.triggerEvent('statechange')
+}
+
+
+function checkCache(readystate, cache, res, ctx) {
   const receive = cache[STATE['HEADERS_RECEIVED']]
-
+  const loading = cache[STATE['LOADING']]
+  const done = cache[STATE['DONE']]
   if (readystate === STATE['HEADERS_RECEIVED']) {
-    const loading = cache[STATE['LOADING']]
-    const done = cache[STATE['DONE']]
-
     if (loading && !loading.done) {
-      context.response = Object.assign(context.response, res)
-      context.event['statechange'].forEach((fn) => fn.call(context))
-
+      handleLoading(loading, ctx)
+    }
+    if (done && !done.done) {
+      handleDone(done, ctx)
     }
   }
+
+  if (readystate === STATE['LOADING']) {
+    if (!receive || !receive.done) {
+      return
+    }
+    if (
+      done
+      && !done.done
+      && String(res).lenght >= String(done.data).lenght
+    ) {
+      handleDone(done, ctx)
+    }
+  }
+}
+
+module.exports = {
+  thunk,
 }
